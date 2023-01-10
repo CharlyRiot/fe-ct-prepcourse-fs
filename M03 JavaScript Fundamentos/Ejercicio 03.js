f975f325340c4c19c5c525c9c2d8725cc0292a88
CharlyRiot <biosistem@gmail.com>
2023-01-10 17:21:59 -0600

    Ejercicios M03 commiteados

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,46 @@ function sonIguales(x, y) {
    // Retorna True si "x" e "y" son iguales.
    // De lo contrario, retorna False.
    // Tu código:
+   return x===y;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna True si los dos strings tienen la misma longitud.
    // De lo contrario, retorna False.
    // Tu código:
+   return str1.length === str2.length;
+   //usamos la propiedad length que retorna el tamaño o número de elementos de un arreglo para asi poder 
+   //comparar su longitud.    
+   //Podemos leerlo como: retorna si es falso o verdadero el resultado de comparar la longitud evaluada por length 
+   //de str1 y str2. 
 }
 
 function menosQueNoventa(num) {
    // Retorna True si el argumento "num" es menor que noventa.
    // De lo contrario, retorna False.
    // Tu código:
+   return (num < 90);
 }
 
 function mayorQueCincuenta(num) {
    // Retorna True si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna False.
    // Tu código:
+   return (num > 50);
 }
 
 function esPar(num) {
    // Retorna True si "num" es par.
    // De lo contrario, retorna False.
    // Tu código:
+   return (num%2 === 0);
 }
 
 function esImpar(num) {
    // Retorna True si "num" es impar.
    // De lo contrario, retorna False.
    // Tu código:
+   return (num%2 != 0);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
